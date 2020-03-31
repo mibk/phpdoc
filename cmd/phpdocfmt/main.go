f5146a98c2c3f9e68e8d6be1072bcf10a043d90a
Michal Bohuslávek <mbohuslavek@gmail.com>
2020-03-31 13:51:06 +0200

    cmd/phpdocfmt: Add bin for formatting PHPDoc comments

---
+++ cmd/phpdocfmt/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+
+	"mibk.io/phpdoc"
+)
+
+var inPlace = flag.Bool("w", false, "write to file")
+
+func main() {
+	flag.Parse()
+	log.SetPrefix("phpdocfmt: ")
+	log.SetFlags(0)
+
+	if flag.NArg() == 0 {
+		if *inPlace {
+			log.Fatal("cannot use -w with standard input")
+		}
+		if err := formatDocs(os.Stdout, os.Stdin); err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+
+	for _, filename := range flag.Args() {
+		f, err := os.Open(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fi, err := f.Stat()
+		if err != nil {
+			log.Fatal(err)
+		}
+		perm := fi.Mode().Perm()
+
+		buf := new(bytes.Buffer)
+		if err := formatDocs(buf, f); err != nil {
+			log.Fatalf("formatting %s: %v", filename, err)
+		}
+		f.Close()
+
+		if *inPlace {
+			// TODO: Make backup file?
+			if err := ioutil.WriteFile(filename, buf.Bytes(), perm); err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			if _, err := io.Copy(os.Stdout, buf); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
+
+var phpdocRx = regexp.MustCompile(`(?s)[ \t]*/\*\*.*?\*/[ \t]*\n?`)
+
+func formatDocs(out io.Writer, in io.Reader) error {
+	data, err := ioutil.ReadAll(in)
+	if err != nil {
+		return err
+	}
+
+	w := &stickyErrWriter{w: out}
+	for len(data) > 0 {
+		loc := phpdocRx.FindIndex(data)
+		if loc == nil {
+			w.Write(data)
+			break
+		}
+
+		m, n := loc[0], loc[1]
+		if m > 0 {
+			w.Write(data[:m])
+		}
+
+		sc := phpdoc.NewScanner(bytes.NewReader(data[m:n]))
+		p := phpdoc.NewParser(sc)
+		if doc, err := p.Parse(); err != nil {
+			log.Println(err)
+			w.Write(data[m:n])
+		} else {
+			// TODO: Check error?
+			if err := phpdoc.Fprint(w, doc); err != nil {
+				log.Println(err)
+			}
+		}
+
+		data = data[n:]
+	}
+	return w.err
+}
+
+type stickyErrWriter struct {
+	w   io.Writer
+	err error
+}
+
+func (w *stickyErrWriter) Write(p []byte) (n int, err error) {
+	if w.err != nil {
+		return 0, w.err
+	}
+	n, w.err = w.w.Write(p)
+	return n, w.err
+}
