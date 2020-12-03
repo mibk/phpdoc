9d977c5a62ce936df9024832189bd6c8184fb0ec
Michal Bohuslávek <mbohuslavek@gmail.com>
2020-12-03 15:02:14 +0100

    _fuzz: Fix build

--- _fuzz/parse_fuzz.go
+++ _fuzz/parse_fuzz.go
@@ -4,18 +4,21 @@ package phpdoc_test
 
 import (
 	"bytes"
+	"io/ioutil"
 
 	"mibk.io/phpdoc"
 )
 
 func Fuzz(b []byte) int {
-	p := phpdoc.NewParser(bytes.NewReader(b))
-	doc, err := p.Parse()
+	doc, err := phpdoc.Parse(bytes.NewReader(b))
 	if err != nil {
 		if doc != nil {
 			panic("doc != nil on error")
 		}
 		return 0
+	}
+	if err := phpdoc.Fprint(ioutil.Discard, doc); err != nil {
+		panic("cannot print")
 	}
 	return 1
 }
